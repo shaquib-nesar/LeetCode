571770a8db7015f9337ef90937dd2d1309ff24be
Shaquib Nesar <nesar.shaquib@gmail.com>
2021-11-06 15:53:32 +0530

    reverted

--- src/com/leet/graphs/FindCenterOfStarGraph.java
+++ src/com/leet/graphs/FindCenterOfStarGraph.java
@@ -3,7 +3,8 @@ package com.leet.graphs;
 public class FindCenterOfStarGraph {
 
     public int findCenter(int[][] edges) {
-        return (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) ?
-                edges[0][0] : edges[0][1];
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            return edges[0][0];
+        return edges[0][1];
     }
 }
